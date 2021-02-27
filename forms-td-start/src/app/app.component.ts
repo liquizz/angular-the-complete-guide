0c8c692c3cdf665998ab37443e003f604e3d2f3e
Vladislav Sheyko <vladislav.sheyko@hotmail.com>
2021-02-27 18:57:37 +0200

    Ended TD forms Module

--- forms-td-start/src/app/app.component.ts
+++ forms-td-start/src/app/app.component.ts
@@ -10,12 +10,45 @@ export class AppComponent {
   @ViewChild('f') f: NgForm;
   defaultQuestion = 'pet';
   answer: string;
+  genders = ['male', 'female'];
+  isSubmitted = false;
+  user = {
+    username: '',
+    email: '',
+    secretQuestion: '',
+    questionAnswer: '',
+    gender: ''
+  };
 
   suggestUserName() {
     const suggestedName = 'Superuser';
+    // this.f.setValue({
+    //   userData: {
+    //     username: suggestedName,
+    //     email: '',
+    //   },
+    //   secret: 'pet',
+    //   questionAnswer: '',
+    //   gender: 'male'
+    // });
+
+    this.f.form.patchValue({
+      userData: {
+        username: suggestedName
+      }
+    });
   }
 
   onSubmit() {
+    this.user.username = this.f.value.userData.username;
+    this.user.email = this.f.value.userData.email;
+    this.user.secretQuestion = this.f.value.secret;
+    this.user.questionAnswer = this.f.value.questionAnswer;
+    this.user.gender = this.f.value.gender;
+    this.isSubmitted = true;
+
     console.log(this.f);
+
+    this.f.reset();
   }
 }
